9577de7aa9e6ad9dad14751a419f92ca599e6d71
GrateCheesus <66963715+GrateCheesus@users.noreply.github.com>
2021-06-30 14:49:59 +0200

    Number Generator setup

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,16 +1,21 @@
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
-    public void test_Given_WeWantToSeeAWelcomeMessage_When_WeGetTheMessage_Then_theMessageShouldBeExactlyHelloRot13(){
+    public void test_Given_WeWantToSeeAWelcomeMessage_When_WeGetTheMessage_Then_theMessageShouldBeExactlyStartingServer(){
         // arrange
-
         // act
         String actual = Main.GetMessage();
-
         // assert
-        Assertions.assertEquals("hello rot13", actual);
+        Assertions.assertEquals("Starting Server", actual);
+    }
+    @Test
+    public void test_Given_WeWantToSeeASuccessMessage_When_WeGetTheMessage_Then_theMessageShouldBeExactlyServerStarted(){
+        // arrange
+        // act
+        String actual = Main.PrintSuccess();
+        // assert
+        Assertions.assertEquals("Server Started", actual);
     }
 }
